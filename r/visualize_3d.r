4ae9740fe7c0103fd201a152d4b2d1148e88c935
Sebastian Deisel <seb.deisel@posteo.de>
2019-06-12 12:47:18 +0200

    added r files (visualize 3d

---
+++ r/visualize_3d.r
@@ -0,0 +1,26 @@
+if(!exists("apirequest", mode="function")) source("./case_study/r/R_Client.R")
+
+
+perform_lookup_and_append_to_df = function(x, y) {
+  input = data.frame(x = x, y = y)
+  apirequest(input, 1, "api-test2D")
+  response = apirequest(input, 1, "api-test2D")
+  
+  return(data.frame(x = x, y = y, result = response))
+}
+
+## new dataframe 
+df <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames <- c("x", "y", "result")
+colnames(df) <- colnames
+
+for(i in 1:100) {
+  x = runif(50, -6, 6)
+  y = runif(50, -6, 6)
+  df = rbind(df, perform_lookup(x, y))
+}
+
+for(i in 1:36) {
+  points3D(df$x, df$y, df$result, theta = i * 10, phi = i, pch = 19, cex = 0.5)
+  Sys.sleep(0.1)
+}
