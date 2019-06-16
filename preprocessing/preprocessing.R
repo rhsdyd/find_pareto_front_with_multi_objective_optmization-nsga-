c0ed2a4906ec2e3ef80e8900484a9e22e88d2933
Sebastian Deisel <seb.deisel@posteo.de>
2019-06-16 16:49:43 +0200

    preprocessing comment

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -2,5 +2,5 @@ normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
-df = load_data_all(1)
-df_normalized = as.data.frame(lapply(df_function_1_all, normalize))
+# df = load_data_all(1)
+# df_normalized = as.data.frame(lapply(df_function_1_all, normalize))
