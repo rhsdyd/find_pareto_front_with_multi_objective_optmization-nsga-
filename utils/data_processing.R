d94380f1bccb3acfd65ecb5d485dd6ab61ce64fe
Sebastian Deisel <seb.deisel@posteo.de>
2019-06-28 12:01:42 +0200

    style

--- utils/data_processing.R
+++ utils/data_processing.R
@@ -2,6 +2,6 @@ normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
-denormalize <- function(x,minval,maxval) {
-  x*(maxval-minval) + minval
+denormalize <- function(x, minval, maxval) {
+  x * (maxval - minval) + minval
 }
