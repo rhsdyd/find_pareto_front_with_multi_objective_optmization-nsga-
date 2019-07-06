4de6155ae7e01eafbb5b71d14898362faf8dec74
rhsdyd <rhsdyd@gmail.com>
2019-07-06 23:27:34 +0200

    test different datasize and track

---
+++ src/test_different_datasize2.R
@@ -0,0 +1,44 @@
+### initi 64 iter 180
+
+multi_objective_result1_2 = multi_objective_optimization(df4_function_1, df4_function_2, 3, store_data_frame_each_iteration = FALSE)
+
+track1_2 = tracking(multi_objective_result1_2)
+
+
+
+
+###  inital 125 iter 150
+
+multi_objective_result2_2 = multi_objective_optimization(df5_function_1, df5_function_2, 150, store_data_frame_each_iteration = FALSE)
+
+track2_2 = tracking(multi_objective_result2_2)
+
+
+
+### inital 216 iter 120
+
+multi_objective_result3_2 = multi_objective_optimization(df6_function_1, df6_function_2, 120, store_data_frame_each_iteration = FALSE)
+
+track3_2 = tracking(multi_objective_result3_2)
+
+
+### inital 343 iter 90
+
+multi_objective_result4_2 = multi_objective_optimization(df7_function_1, df7_function_2, 90, store_data_frame_each_iteration = FALSE)
+
+track4_2 = tracking(multi_objective_result4_2)
+
+### inital 512 iter 60
+
+multi_objective_result5_2 = multi_objective_optimization(df8_function_1, df8_function_2, 60, store_data_frame_each_iteration = FALSE)
+
+track5_2 = tracking(multi_objective_result5_2)
+
+
+### inital 729 iter 30
+
+multi_objective_result6_2 = multi_objective_optimization(df9_function_1, df9_function_2, 30, store_data_frame_each_iteration = FALSE)
+
+track6_2 = tracking(multi_objective_result6_2)
+
+
