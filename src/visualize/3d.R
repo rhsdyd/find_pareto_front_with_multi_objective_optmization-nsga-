ddd8af68527694e3825bf4376fc207e15a258e9f
Sebastian Deisel <seb.deisel@posteo.de>
2019-07-08 16:31:34 +0200

    tes

--- src/visualize/3d.R
+++ src/visualize/3d.R
@@ -11,6 +11,7 @@ visualize_3D = function (df, from = 1, to = nrow(df), main = "") {
   )
 }
 
+
 par(mfrow = c(1,2))
 visualize_3D(df_function_1, 1, 407, "Initial Dataset Function 1")
 visualize_3D(df_function_2, 1, 407, "Initial Dataset Function 2")
