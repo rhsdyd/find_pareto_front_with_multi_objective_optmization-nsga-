03a42d29193db4467f6148e9883a02de4207266f
livengooddaily <eddie.livengood@gmail.com>
2019-07-08 16:42:03 +0200

    Initial dataset visualization

--- src/visualize/3d.R
+++ src/visualize/3d.R
@@ -14,3 +14,11 @@ visualize_3D = function (df, from = 1, to = nrow(df), main = "") {
 par(mfrow = c(1,2))
 visualize_3D(df_function_1, 1, 407, "Initial Dataset Function 1")
 visualize_3D(df_function_2, 1, 407, "Initial Dataset Function 2")
+
+#Visualize Initial Dataset
+f1.data = read.csv("data/df_1_6th_loop.csv",header = T)
+f2.data = read.csv("data/df_2_6th_loop.csv",header = T)
+par(mfrow=c(1,2))
+scatter3D(f1.data$x[1:407],f1.data$y[1:407],f1.data$z[1:407],colvar=f1.data$output[1:407],pch = 20, main = "Function", clab="Output", phi = 20, theta = 70)
+scatter3D(f2.data$x[1:407],f2.data$y[1:407],f2.data$z[1:407],colvar=f2.data$output[1:407],pch = 20, main = "Function 2", clab="Output", phi = 20, theta = 70)
+mtext("Initial Data",outer = TRUE, cex = 2)
