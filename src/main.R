fc75c3a3d65ef9cf852373e782c8522739ab5bde
Sebastian Deisel <seb.deisel@posteo.de>
2019-07-09 14:38:53 +0200

    clean up main

--- src/main.R
+++ src/main.R
@@ -1,19 +1,7 @@
 setwd("case_study/")
 
+source("src/api/api_client.R")
+source("src/fetch/init_fetch.R")
 source("src/multi_objective/multi_objective.R")
 
-multi_objective_result = multi_objective_optimization(df_function_1, df_function_2,100, store_data_frame_each_iteration = FALSE)
-
-multi_objective_result$df_1
-multi_objective_result$df_2
-multi_objective_result$history_models_pareto_front
-multi_objective_result$non_dominated_sorting_history
-
-
-domsort = multi_objective_result$non_dominated_sorting_history
-
-plot(domsort[[1]], ylim=c(0, 100), xlim=c(0, 3000))
-for(i in 2:length(domsort)) {
-  points(domsort[[i]], col=i)
-  Sys.sleep(1)
-}
+multi_objective_optimization_result = multi_objective_optimization(df_function_1, df_function_2, 150, store_data_frame_each_iteration = FALSE)
